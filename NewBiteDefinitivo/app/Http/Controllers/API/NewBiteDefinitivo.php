77d9cf2b75128169fcfa04cadf21acab8f34b3fd
Sparkion01 <cauabvcv523@gmail.com>
2023-11-15 16:08:57 -0300

    backend

---
+++ NewBiteDefinitivo/app/Http/Controllers/API/NewBiteDefinitivo.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\NewBiteDefinitivo;
+use Illuminate\Http\Request;
+
+class NewBiteDefinitivo extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NewBiteDefinitivo $newBiteDefinitivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, NewBiteDefinitivo $newBiteDefinitivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NewBiteDefinitivo $newBiteDefinitivo)
+    {
+        //
+    }
+}
